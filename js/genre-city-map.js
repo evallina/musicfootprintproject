85afaf6c6520a196f5fc6fdfda35fd153cfb6a27
Enol Vallina <enol.vallina@outlook.com>
2016-12-13 09:49:12 -0500

    map link change

--- js/genre-city-map.js
+++ js/genre-city-map.js
@@ -80,8 +80,8 @@ VenueMap.prototype.initVis = function() {
     vis.map = L.map(vis.parentElement)
         .setView(vis.mapCenter, 13);
 
-    L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
-        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
+        attribution: 'Map tiles by <a href="https://stamen.com">Stamen Design</a>, <a href="https://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
         subdomains: 'abcd',
         minZoom: 0,
         maxZoom: 20,
